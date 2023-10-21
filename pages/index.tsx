19813aa4ed7338330e92ae5d4706bab3fcbe0211
orangekame3 <miya.org.0309@gmail.com>
2023-10-21 16:05:48 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Button, Group } from "@mantine/core";
 export default function IndexPage() {
   return (
     <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
+      <Button size="xl">HI Welcome to Mantine!</Button>
     </Group>
   );
 }
