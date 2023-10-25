64ea369a5e0a1a3ba5cc4e1ee5aac918ff35c8d3
Takafumi Miyanaga <miya.org.0309@gmail.com>
2023-10-25 13:11:24 +0900

    index.tsx の更新

--- pages/index.tsx
+++ pages/index.tsx
@@ -254,7 +254,7 @@ function Publication(){
 		{ title: "tftarget", description: "🎯tftarget is a CLI tool for Terraform ( plan | apply | destroy ) with target option. You can interactivity select resource to ( plan | apply | destroy ) with target option.",link:"https://github.com/future-architect/tftarget",star:175},
 		{ title: "paclear", description: "👾paclear is a clear command with PAC-MAN animation👾",link:"https://github.com/orangekame3/paclear",star:146},
 		{ title: "stree", description: "📁Directory trees of AWS S3 Bucket",link:"https://github.com/orangekame3/stree",star:44},
-		{ title: "ghfetch", description: "ghfetch is a CLI tool to fetch GitHub user information and show like neofetch.",link:"https://github.com/orangekame3/ghfetch",star:17},
+		{ title: "ghfetch", description: "ghfetch is a CLI tool to fetch GitHub user information and show like neofetch.",link:"https://github.com/orangekame3/ghfetch",star:19},
 	];
 
 	function OSS(){
